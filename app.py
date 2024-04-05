4ee0582bdcd9cebbdd446d370ec1d54a56b5e121
Greg Lemrow <greg.lemrow@gmail.com>
2024-04-05 00:14:10 -0400

    cleaning up app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 
 import aws_cdk as cdk
 
-# from rose_network.vpc import CourtOfRosesNetwork
 from rose_pipeline.pipeline import RosePipeline
 
 
@@ -11,7 +10,6 @@ app = cdk.App()
 
 env_primary = cdk.Environment(account="058264153331", region="us-east-1")
 
-# CourtOfRosesNetwork(app, "CourtOfRosesNetwork", env=env_primary)
 RosePipeline(app, "RosePipeline", env=env_primary)
 
 app.synth()
